fa6c71795b69ff621757284155f1004a799fb0e6
PalaniBaba <palanibaba2002@gmail.com>
2023-10-21 17:46:29 +0530

    Initial Commit

---
+++ MakeConnection/MakeConnectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeConnectionApp.swift
+//  MakeConnection
+//
+//  Created by Palani on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MakeConnectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
