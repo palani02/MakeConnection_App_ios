ffbf9a89aed9f0c512f92b6bf2b188973c2f9bce
palani02 <palanibaba2002@gmail.com>
2023-10-22 22:40:43 +0530

    Make User Authentication and Added Firebase

---
+++ MakeConnection/LoginPage.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPage.swift
+//  MakeConnection
+//
+//  Created by Palani on 22/10/23.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginPage()
+}
